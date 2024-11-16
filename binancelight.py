16e0611ec4d2c2db050b7fbddf82d5d1cff0dec5
skhnpark <skhnpark125@gmail.com>
2024-11-16 16:15:54 +0900

    feat: 로그 코드 분리

--- binancelight.py
+++ binancelight.py
@@ -1,29 +1,8 @@
-import logging
-import os
-
 from binance.spot import Spot
 from inspect import stack
+from logger import Logger
 
-
-current_path = os.path.dirname(os.path.abspath(__file__))
-file_name = os.path.splitext(os.path.basename(__file__))[0] + ".log"
-file_path = str(os.path.join(current_path, file_name))
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.ERROR)
-
-formatter = logging.Formatter(
-    fmt="[%(asctime)s.%(msecs)03d] %(levelname)s %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S"
-)
-
-file_handler = logging.FileHandler(filename=file_path, mode="a", encoding="UTF-8")
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
-
-stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(formatter)
-logger.addHandler(stream_handler)
+logger = Logger("binance-light")
 
 
 def _handle_exception(e):
